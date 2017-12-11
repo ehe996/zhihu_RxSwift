00f35ba3e499d38973a1f794465f25e3e3f5b17e
Andy He <928510851@qq.com>
2017-12-11 22:09:08 +0800

    初始化工程

---
+++ ZhiHuDaily/ZhiHuDaily/ViewControllers/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  ZhiHuDaily
+//
+//  Created by Andy He on 2017/12/10.
+//  Copyright © 2017年 FlyingFishHe. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var bannerView: BannerView!
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
